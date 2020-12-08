bc3b70ac691677abfbe47cc53314e4bbcc888d19
Ngoc Trai <trailaanh@gmail.com>
2020-12-08 15:11:42 +0700

    add

--- php/login.php
+++ php/login.php
@@ -12,6 +12,7 @@
         $result = pg_query($query);
         if($result)
         {
+            echo (pg_result($result, 0, "password"));
             $db_password = pg_result($result, 0, "password");
             $db_fullname = pg_result($result, 0, "fullname");
             $db_phone = pg_result($result, 0, "phone");
