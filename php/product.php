559b9f46230a7db3f180361360333a4c82c4dcd5
Ngoc Trai <trailaanh@gmail.com>
2020-12-15 13:55:51 +0700

    add

--- php/product.php
+++ php/product.php
@@ -1,7 +1,7 @@
 <?php>
-include ('database.php');
+include("database.php");
 if(getdb()){
-    $query="SELECT * FROM product";
+    $query="SELECT * FROM productlist";
     $result=pg_query($query);
 
     if($result){
