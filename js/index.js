c9ca4ea185f54646f43cc673119861fbb274573d
Ngoc Trai <trailaanh@gmail.com>
2020-12-17 01:01:09 +0700

    add

--- js/index.js
+++ js/index.js
@@ -85,7 +85,7 @@ function showProduct(products){
     for(item of products){
         item.price = numberWithCommas(item.price);   
         var text = `
-        <div class="card" style="width: 18rem">
+        <div class="card" style="width: 18rem; padding: 0 10px;">
             <img src="${item.img}"class="card-img-top"alt="${item.name}"/>
             <div class="card-body">
               <h3 class="card-title">${item.name}</h3>
