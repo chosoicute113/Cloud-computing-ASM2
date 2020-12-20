069a1071e50d9f8301b0d4090548cc5892f8794f
Ngoc Trai <trailaanh@gmail.com>
2020-12-21 04:24:59 +0700

    add

--- php/prodct_category.php
+++ php/prodct_category.php
@@ -1,8 +1,8 @@
 <?php
-if (isset($_POST['id'])) {
+if (isset($_POST['CATEGORY'])) {
     include('database.php');
     if ( getDb() ) {
-        $id = $_POST['id'];
+        $category = $_POST['CATEGORY'];
 
         $query=
         "SELECT *  FROM PRODUCT,SUB_PRODUCT,SUB_CATEGORY,CATEGORY WHERE PRODUCT.id = SUB_PRODUCT.product_id AND SUB_PRODUCT.sub_id = SUB_CATEGORY.id AND SUB_CATEGORY.cate_id = CATEGORY.id AND CATEGORY.name = '$category'";
