031917a5de552f1be8e0909ec0c7bbf16069def9
Ngoc Trai <trailaanh@gmail.com>
2020-12-21 04:23:09 +0700

    add

--- php/prodct_category.php
+++ php/prodct_category.php
@@ -4,11 +4,8 @@ if (isset($_POST['id'])) {
     if ( getDb() ) {
         $id = $_POST['id'];
 
-        $query="SELECT *  FROM PRODUCT,SUB_PRODUCT,SUB_CATEGORY,CATEGORY
-                WHERE PRODUCT.id = SUB_PRODUCT.product_id
-                AND SUB_PRODUCT.sub_id = SUB_CATEGORY.id
-                AND SUB_CATEGORY.cate_id = CATEGORY.id
-                AND CATEGORY.name = '$category'";
+        $query=
+        "SELECT *  FROM PRODUCT,SUB_PRODUCT,SUB_CATEGORY,CATEGORY WHERE PRODUCT.id = SUB_PRODUCT.product_id AND SUB_PRODUCT.sub_id = SUB_CATEGORY.id AND SUB_CATEGORY.cate_id = CATEGORY.id AND CATEGORY.name = '$category'";
         $result = pg_query($query);
 
         if ($result) {
