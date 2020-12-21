0317191f0674478fbdae9d1a6ce2d6a9e30a87ec
Ngoc Trai <trailaanh@gmail.com>
2020-12-22 02:11:46 +0700

    add

--- php/login.php
+++ php/login.php
@@ -2,7 +2,7 @@
     $username = $_POST['username'];
     $password = $_POST['password'];
     $success =0;
-    $db_fullname= $db_phone= $db_birthday= $db_age ="";
+    $db_username = $db_fullname= $db_phone= $db_birthday= $db_age ="";
     include("database.php");
     $db = getDb();
 
@@ -12,6 +12,7 @@
         $result = pg_query($query);
         if($result)
         {
+            $db_username = pg_result($result, 0, "username");
             $db_password = pg_result($result, 0, "password");
             $db_fullname = pg_result($result, 0, "fullname");
             $db_phone = pg_result($result, 0, "phone");
@@ -27,6 +28,7 @@
     echo json_encode(array('success' => $success,
                            'fullname' => $db_fullname,
                            'phone' => $db_phone,
+                           'username' => $db_username,
                            'birthday' => $db_birthday,
                            'age' => $db_age));
 ?>
